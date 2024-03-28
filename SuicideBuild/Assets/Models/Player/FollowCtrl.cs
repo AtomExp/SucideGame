59bbf8e9cbcc79150b94a449c64608cefadd0e4a
Waysidechimp <67596048+Waysidechimp@users.noreply.github.com>
2024-03-28 16:04:57 -0400

    Working On Model Tracking

---
+++ SuicideBuild/Assets/Models/Player/FollowCtrl.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCtrl : MonoBehaviour
+{
+
+    [SerializeField] GameObject followObj;
+    [SerializeField] bool followPos, followRot;
+    private void Update()
+    {
+        if (followPos)
+        {
+            this.transform.position = followObj.transform.position;
+        }
+        if (followRot)
+        {
+            this.transform.rotation = followObj.transform.rotation;
+        }
+    }
+}
